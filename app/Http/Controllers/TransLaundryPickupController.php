8b3c418bbbbb1500da098cbd9daad7b2c8d0bc06
milabo123 <bebeklari345@gmail.com>
2026-04-14 15:01:08 +0700

    first commit

---
+++ app/Http/Controllers/TransLaundryPickupController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trans_laundry_pickup;
+use Illuminate\Http\Request;
+
+class TransLaundryPickupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Trans_laundry_pickup $trans_laundry_pickup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Trans_laundry_pickup $trans_laundry_pickup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Trans_laundry_pickup $trans_laundry_pickup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Trans_laundry_pickup $trans_laundry_pickup)
+    {
+        //
+    }
+}
---
+++ app/Models/Trans_laundry_pickup.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trans_laundry_pickup extends Model
+{
+    //
+}
